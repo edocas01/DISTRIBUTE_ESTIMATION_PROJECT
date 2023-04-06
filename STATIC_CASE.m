ba06684338a26ff2fa72289812ee77cc75786615
massimilianopranzo <massimiliano.pranzo@studenti.unitn.it>
2023-04-06 14:24:21 +0200

    plot entities

--- STATIC_CASE.m
+++ STATIC_CASE.m
@@ -1,49 +1,16 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 clc
 close all
 clearvars
 
 addpath("Classes\")
+addpath("Functions\")
 
-%% Classes
-% ROBOT class 
-% classdef ROBOT
-% 	properties
-% 		x           % x location
-%         y           % y location
-%         ComRadius    % Communication radius
-% 	end
-% 	methods 
-%         % Define the position
-%         function obj = setPosition(obj, valx, valy)
-% 			obj.x = valx;
-% 			obj.y = valy;
-%         end        
-%         % Define communication radius
-%         function obj = setComRadius(obj, val_radius)
-% 			obj.ComRadius = val_radius;
-% 		end			
-% 	end	
-% end
-
-% TARGET class
-% classdef TARGET
-% 	properties
-% 		  x           % x location
-%         y           % y location
-%         ComRadius    % Communication radius
-% 	end
-% 	methods 
-%         % Define the position
-%         function obj = setPosition(obj, valx, valy)
-% 			obj.x = valx;
-% 			obj.y = valy;
-%         end       
-%         % Define communication radius
-%         function obj = setComRadius(obj, val_radius)
-% 			obj.ComRadius = val_radius;
-% 		end			
-% 	end	
-% end
+rng('default');
 
 %% Entities definition
 % Target
@@ -52,8 +19,8 @@ y_T = 0;
 ComRadius_T = 1;
 
 T = TARGET();
-T.setPosition(x_T, y_T);
-T.setComRadius(ComRadius_T);
+T = T.setPosition(x_T, y_T);
+T = T.setComRadius(ComRadius_T);
 
 % Robots
 n_robot = 6;
@@ -61,12 +28,12 @@ xR_min = -5;
 xR_max = 5;
 yR_min = -5;
 yR_max = 5;
-ComRadius_R = rand(1, n_robot); % Communication radius array 
+ComRadius_R = 1 + rand(1, n_robot); % Comunication radii [1 - 2] 
 
 for i = 1 : n_robot
 	R.(strcat("R",num2str(i))) = ROBOT();
 	R.(strcat("R",num2str(i))) = R.(strcat("R",num2str(i))).setPosition(xR_min + (xR_max - xR_min) * rand(), yR_min + (yR_max - yR_min) * rand());
-	R.(strcat("R",num2str(i))).setComRadius(ComRadius_R(i));
+	R.(strcat("R",num2str(i))) = R.(strcat("R",num2str(i))).setComRadius(ComRadius_R(i));
 end
 
 %% Plot the entities
@@ -78,13 +45,13 @@ axis([-6 6 -6 6])
 xlabel("x")
 ylabel("y")
 title("Entities")
-plot(T.x, T.y, 'o', 'MarkerSize', 10, 'MarkerFaceColor', 'r')
+plot(T.x, T.y, 'o', 'MarkerSize', 5, 'MarkerFaceColor', 'r')
+Circle(T.x, T.y, T.ComRadius)
 for i = 1 : n_robot
-	plot(R.(strcat("R",num2str(i))).x, R.(strcat("R",num2str(i))).y, 'o', 'MarkerSize', 10, 'MarkerFaceColor', 'b')
+    plot(R.(strcat("R",num2str(i))).x, R.(strcat("R",num2str(i))).y, 'o', 'MarkerSize', 5, 'MarkerFaceColor', 'b')
+    	Circle(R.(strcat("R",num2str(i))).x, R.(strcat("R",num2str(i))).y, R.(strcat("R",num2str(i))).ComRadius)
 end
-
-
-
+hold off
 
 
 
