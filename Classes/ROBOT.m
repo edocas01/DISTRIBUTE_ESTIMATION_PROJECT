198a2053b2a75c214e40f7c5dde21b264d3ec907
edoardo <edoardo.castioni@gmail.com>
2023-04-11 21:33:08 +0200

    starting classes

--- Classes/ROBOT.m
+++ Classes/ROBOT.m
@@ -1,21 +1,79 @@
-classdef ROBOT
+classdef ROBOT < handle
+
+%{
+
+ 
+      _   _   _        _ _           _            
+     / \ | |_| |_ _ __(_) |__  _   _| |_ ___  ___ 
+    / _ \| __| __| '__| | '_ \| | | | __/ _ \/ __|
+   / ___ \ |_| |_| |  | | |_) | |_| | ||  __/\__ \
+  /_/   \_\__|\__|_|  |_|_.__/ \__,_|\__\___||___/
+                                                  
+ 
+
+%}
+
 	properties
-		x           % x location
-        y           % y location
-        ComRadius   % Communication radius
+		x 					% state vector
+		P       		% covariance matrix of the state
+		x_est 			% estimated position
+		ComRadius 	% communication radius
+
+		id 					% id of the robot
+		maxInput		% maximum input
 	end
+
+%{
+
+ 
+   ____        _     _ _        __  __                _                   
+  |  _ \ _   _| |__ | (_) ___  |  \/  | ___ _ __ ___ | |__   ___ _ __ ___ 
+  | |_) | | | | '_ \| | |/ __| | |\/| |/ _ \ '_ ` _ \| '_ \ / _ \ '__/ __|
+  |  __/| |_| | |_) | | | (__  | |  | |  __/ | | | | | |_) |  __/ |  \__ \
+  |_|    \__,_|_.__/|_|_|\___| |_|  |_|\___|_| |_| |_|_.__/ \___|_|  |___/
+                                                                          
+ 
+
+%}
+
 	methods 
-        % Define the position
-        function obj = setPosition(obj, valx, valy)
-			obj.x = valx;
-			obj.y = valy;
-        end
-        
-        % Define communication radius
-        function obj = setComRadius(obj, val_radius)
-			obj.ComRadius = val_radius;
+		% Iniatialization of the robot
+    function obj = Robot(x, y, comradius)
+			obj.x(1) = x;
+			obj.x(2) = y;
+			obj.ComRadius = comradius;
+			obj.P = eye(2);
+    end
+    
+		% Update the position of the robot
+		function obj = dynamics(obj, u)
+			% linear dynamics
+			obj.x(1) = obj.x(1) + u(1);
+			obj.x(2) = obj.x(2) + u(2);
 		end
-			
+
+%{
+
+ 
+   ____       _            _         __  __                _                   
+  |  _ \ _ __(_)_   ____ _| |_ ___  |  \/  | ___ _ __ ___ | |__   ___ _ __ ___ 
+  | |_) | '__| \ \ / / _` | __/ _ \ | |\/| |/ _ \ '_ ` _ \| '_ \ / _ \ '__/ __|
+  |  __/| |  | |\ V / (_| | ||  __/ | |  | |  __/ | | | | | |_) |  __/ |  \__ \
+  |_|   |_|  |_| \_/ \__,_|\__\___| |_|  |_|\___|_| |_| |_|_.__/ \___|_|  |___/
+                                                                               
+ 
+
+%}
+
+	function output = compute_control()
+		
 	end
+
+	function GPS_meas = GPS_measurement(obj)
+
+	end
+
+			
+	end % methods
 	
-end
+end % classdef
---
+++ config.m
@@ -0,0 +1,8 @@
+parameters_conf = struct( ...
+  'dt', 0.1, ...   % time step
+  'tmax', 100, ... % simulation time
+  ...
+  'R_gps', 
+
+
+);
