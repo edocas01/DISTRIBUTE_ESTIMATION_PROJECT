d1ac60a23975a1d00aff07e504717ca60d5e97df
massimilianopranzo <massimiliano.pranzo@studenti.unitn.it>
2023-04-15 15:41:22 +0200

    visualize trajectory while creating it and set all vectors as 2 by n (large rectangualar) @edocas01

--- main.m
+++ main.m
@@ -14,33 +14,36 @@ if N > parameters_simulation.N_MAX
 end
 
 
-%% Test robot
+%% Assign trajectory
 % robot needs x0,y0,comradius,id,type
 robot = ROBOT(0,0,0.1,1,'linear');
 target = TARGET(0,0);
 u_target = target_trajectory(parameters_simulation);
 
 % time = 0:parameters_simulation.dt:parameters_simulation.tmax;
-time = 0:parameters_simulation.dt:length(u_target(:,1));
-for t = 1:length(u_target(:,1))
-	u = [0.001;0.001];
-	EKF(robot, u);
+time = 0 : parameters_simulation.dt : length(u_target);
+x_true = zeros(2, length(u_target));
+x_est = zeros(2, length(u_target));
+traj = zeros(2, length(u_target));
 
-	err(t,:) = norm(robot.x - robot.x_est);
-    x_true(t,:) = robot.x;
-    x_est(t,:) = robot.x_est;
+for t = 1:length(u_target)
+    target.dynamics(u_target(:,t));
+	EKF(robot, u_target(:,t));
 
-    target.dynamics(u_target(t,:));
-    traj(t,:) = target.x;
+	% err(t,:) = norm(robot.x - robot.x_est);
+    x_true(:,t) = robot.x;
+    x_est(:,t) = robot.x_est;
+    traj(:,t) = target.x;
+
 
 end
 
-
-plot(x_true(:,1),x_true(:,2),'-k')
+figure(2); clf
+plot(x_true(1,:),x_true(2,:),'-k')
 hold on
-plot(x_est(:,1),x_est(:,2),'-b')
+plot(x_est(1,:),x_est(2,:),'-b')
 hold on
-plot(traj(:,1),traj(:,2),'-r')
+plot(traj(1,:),traj(2,:),'-r')
 
 
 
