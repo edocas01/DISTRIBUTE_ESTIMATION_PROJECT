f09ad1b45c8e69a98a6aed381b4340f66b8a4281
edoardo <edoardo.castioni@gmail.com>
2023-04-28 11:20:32 +0200

    updated readme

--- Scripts/test_voronoi.m
+++ Scripts/test_voronoi.m
@@ -9,29 +9,33 @@ for i = 1:points
 	y(i) = (rand() - 0.5)*range;
 end
 
-% Plot the points
-plot(x, y, 'or');
-hold on
 P = [x', y'];
 % Find the voroni map
-[v,c] = voronoin(P);
-
-% Plot the voroni map for the a generic point
-idx = round(rand()*points);
-for idx = 1:points
-for i = 1:length(c{idx})
-	plot(v(c{idx}(i),1), v(c{idx}(i),2), 'b*');
-end
-end
+[vin,cin] = voronoin(P);
+[vx,vy] = voronoi(x,y);
 
 figure()
-[a,b] = voronoi(x,y);
-plot(a,b,'b')
+plot(vx,vy,'b')
 hold on 
 plot(x, y, 'or');
+plot(vin(:,1), vin(:,2), 'ok')
+% define of the vertex of voronoi in a matrix
+v = zeros(length(vx(1,:))*2,2);
+v = [vx(1,:)', vy(1,:)'; vx(2,:)', vy(2,:)'];
+% remove the duplicate points
+v = unique(v, 'rows');
+plot(v(:,1), v(:,2), '.g');
 
+% remove the first element of vin if it's infinite
+vin_noinf = vin;
+cin_noinf = cin;
+if (isinf(vin(1,1)) || isinf(vin(1,2)))
+	vin_noinf(1,:) = [];
+	for i = 1:length(cin)
+		cin_noinf{i}(find(cin{i} == 1)) = [];
+	end
+end
 
+% find the points that are not conisdered in vin_noinf but are in v
 
-dt = delaunayTriangulation(P);
-[V,R] = voronoiDiagram(dt);
 
