fb07b6af6fec5a928e15bab7ea018e53be6a0293
massimilianopranzo <massimiliano.pranzo@studenti.unitn.it>
2023-05-09 10:00:23 +0200

    minor changes

--- Scripts/test_ellipse_voronoi.m
+++ Scripts/test_ellipse_voronoi.m
@@ -1,10 +1,11 @@
 clc; clear; 
 addpath('Functions')
 
-P1 = [0.5, 0.3;
-     0.3, 0.5];
-P2 = [0.5, 0.2;
-     0.2, 0.5];
+P1 = [2, 0.3;
+     0.3, 3]; % Covariance matrix of 
+
+P2 = [10, 0.2;
+     0.2, 4];
 
 c1 = [1; 3];
 [pts1, rx1, ry1, th1] = ellipse(c1, P1);
@@ -27,9 +28,12 @@ c2_in1 = Rth1' * (c2 - c1);
 dir = c2_in1;
 alpha = mod(atan2(dir(2), dir(1)), 2 * pi); % Real angle at which c2 is seen by c1
 phi = atan(rx1 / ry1 * tan(alpha)); % Fake angle to get the correct radius
-dist = sqrt(rx1^2 * cos(phi)^2 + ry1^2 * sin(phi)^2);
+% dist = sqrt(rx1^2 * cos(phi)^2 + ry1^2 * sin(phi)^2);
 gain = 1;
-pt_inter1 = c1 + Rth1 * gain * dist * [cos(alpha); sin(alpha)];
+pt_inter1 = c1 + Rth1 * gain * [rx1 * cos(phi); ry1 * sin(phi)];
+
+norm(c1 - pt_inter1)
+sqrt((rx1 * cos(phi))^2 + (ry1 * sin(phi))^2)
 
 % Ellipse 2
 c1_in2 = Rth2' * (c1 - c2);
@@ -54,5 +58,6 @@ plot(pt_inter2(1), pt_inter2(2), 'k*')
 plot([c1(1), pt_inter1(1)], [c1(2), pt_inter1(2)], 'r-')
 plot([c2(1), pt_inter2(1)], [c2(2), pt_inter2(2)], 'b-')
 hold off
+
 
 
