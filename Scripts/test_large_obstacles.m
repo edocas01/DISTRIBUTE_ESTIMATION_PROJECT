3ef6e5b3530cfc1fdb8aab9cb840c673b04a44bb
edoardo <edoardo.castioni@gmail.com>
2023-09-16 23:07:51 +0200

    working on LO

--- Scripts/test_large_obstacles.m
+++ Scripts/test_large_obstacles.m
@@ -86,8 +86,8 @@ if ~isempty(intersection) % there is intersection
 				for j = 1:size(intersection,1)
 					dir = intersection(:,j) - [0;0];
 					dir = dir/norm(dir);
-					% create a point 10 meters behind the intersection
-					points_to_delete = [points_to_delete, [intersection(:,j) + 10*dir , intersection(:,j)]];
+					% create a point 1000 meters behind the intersection
+					points_to_delete = [points_to_delete, [intersection(:,j) + 1000*dir , intersection(:,j)]];
                 end
                 plot(points_to_delete(1,:),points_to_delete(2,:),'ok');
 				% reorder the points to delete and create a polyshape
@@ -100,37 +100,31 @@ if ~isempty(intersection) % there is intersection
 		for i = 1:length(region_to_delete)
 			poly_voronoi = subtract(poly_voronoi, region_to_delete{i});
 		end
+
 		clf;
 		hold on;
 		grid on;
 		axis equal;
 		axis(10 * [-1 1 -1 1]);
 		plot(poly_voronoi)
-	else
-
+		% LO.plot();
+	else % there are visible vertices in the voronoi cell
+		% find, if any, the intersection between the obstacle and the voronoi cell
+		intersection = [];
+        pp = [];
+		LO_vertices = LO.poly.Vertices;
+		N_vertices = size(LO_vertices,1);
+		% add the first vertex at the end to close the polygon
+		LO_vertices = LO_vertices([1:N_vertices,1],:);
+		for i = 1:N_vertices
+			intersection = linexlines2D(poly_voronoi, LO_vertices(i,:), LO_vertices(i+1,:));
+			pp = [pp, intersection];
+            plot(intersection(1,:),intersection(2,:),'og','MarkerFaceColor','g');
+		end
+        
+		for i = 1:size(visible_points,1)
+			plot(visible_points(i,1), visible_points(i,2), 'or');
+            pp = [pp, visible_points(i,:)'];
+		end
 	end
 end
-
-% 
-% pgon=polyshape(     [2.8259    1.8997
-%     1.3496    4.4206
-%     3.3552    3.4178
-%     4.3719    4.6992
-%     4.8872    2.1226
-%     3.2298    2.7214
-%     3.3134    1.1337
-%     1.1685    1.1616] );  %hypothetical polygon
-% R=1.5; [x0,y0]=deal(3,3); %Circle radius and center
-% t=linspace(0,360,1000).'; t(end)=[]; %circle angular samples
-% circle=polyshape([cosd(t), sind(t)]*R+[x0,y0]); 
-% plot([pgon,circle]); axis equal
-% %find intersections
-% V=pgon.Vertices;
-% N=size(V,1);
-% V=V([1:N,1],:);
-% hold on
-% for i=1:N
-%     xy=linexlines2D(circle,V(i,:),V(i+1,:));
-%     plot(xy(1,:),xy(2,:),'or','MarkerFaceColor','r');
-% end
-% hold off
